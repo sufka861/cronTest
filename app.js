72af1120627deae4f494db3b39c96310a4286343
Suf <sufkarmon2@gmail.com>
2022-12-10 15:43:58 +0200

    comit 4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { schedule } = require("node-cron");
 
 cron.schedule("* * * * *", function(){
     console.log("starting cron function ------");
-    const time = new Date('05 October 2011 14:48 UTC');
+    const time = new Date.now();
     console.log("time in string: " + time.toString());
     console.log("time in ISO: " + time.toISOString());
 })
